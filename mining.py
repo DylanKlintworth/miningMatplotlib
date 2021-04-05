75e2f8087f2cebcfb02c2433fe9b4d9ce72fade6
Dylan Klintworth <dylan.klintworth@gmail.com>
2021-04-05 16:53:22 -0500

    added get_issues_over_time()

--- mining.py
+++ mining.py
@@ -1,5 +1,5 @@
 import github as gh
-
+import datetime
 
 def get_repo(github):
     """Returns the Repository object representation of the matplotlib repository"""
@@ -13,7 +13,7 @@ def get_all_issues(repo):
     return issues
 
 
-def get_issues_over_time(issues):
+def get_issues_over_time(issues, query):
     """Return a dictionary in which each key represents a timeframe (day, week, month, year, 5 years)
     and holds the value of a list of issues that have been created within that timeframe"""
     time = {
@@ -21,5 +21,27 @@ def get_issues_over_time(issues):
         "week": list(),
         "month": list(),
         "year": list(),
-        "5_years": list()
+        "five_years": list()
     }
+    date = datetime.datetime.today()
+    day = date - datetime.timedelta(days=1)
+    week = date - datetime.timedelta(weeks=1)
+    month = date - datetime.timedelta(weeks = 4)
+    year = date - datetime.timedelta(weeks=52)
+    five_years = date - datetime.timedelta(weeks=(52*5))
+    for issue in issues:
+        if issue.state == query:
+            created_at = issue.created_at
+            if created_at >= day:
+                time["day"].append(issue)
+            elif day >= created_at >= week:
+                time["week"].append(issue)
+            elif week >= created_at >= month:
+                time["month"].append(issue)
+            elif month >= created_at >= year:
+                time["year"].append(issue)
+            elif year >= created_at >= five_years:
+                time["five_years"].append(issue)
+    return time
+
+
