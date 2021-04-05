0cf62529134c4e708966ae6bf4137c713d6f22da
Dylan Klintworth <dylan.klintworth@gmail.com>
2021-04-05 17:00:56 -0500

    added function documentation

--- miningUtilities.py
+++ miningUtilities.py
@@ -1,21 +1,39 @@
 import github as gh
 import datetime
 
+
 def get_repo(github):
-    """Returns the Repository object representation of the matplotlib repository"""
+    """
+    Returns the Repository object representation of the matplotlib repository
+
+        Parameters:
+            github (github.Github): The main GitHub object representation
+    """
     repo = github.get_repo("matplotlib/matplotlib")
     return repo
 
 
 def get_all_issues(repo):
-    """Returns a PaginatedList of Issue objects (open/closed) sorted by when they were created"""
+    """
+    Returns a PaginatedList of Issue objects (open/closed) sorted by when they were created
+
+        Parameters:
+                repo (github.Repository): The repository selected to get the issues of
+    """
     issues = repo.get_issues(state="all", sort="created")
     return issues
 
 
 def get_issues_over_time(issues, query):
-    """Return a dictionary in which each key represents a timeframe (day, week, month, year, 5 years)
-    and holds the value of a list of issues that have been created within that timeframe"""
+    """
+    Return a dictionary in which each key represents a timeframe (day, week, month, year, 5 years)
+    and holds the value of a list of issues that have been created within that timeframe
+
+        Parameters:
+                issues (PaginatedList of Issues): the repositories' issues
+                query (str): The state to be queries e.g "open"/"closed"
+
+    """
     time = {
         "day": list(),
         "week": list(),
@@ -26,9 +44,9 @@ def get_issues_over_time(issues, query):
     date = datetime.datetime.today()
     day = date - datetime.timedelta(days=1)
     week = date - datetime.timedelta(weeks=1)
-    month = date - datetime.timedelta(weeks = 4)
+    month = date - datetime.timedelta(weeks=4)
     year = date - datetime.timedelta(weeks=52)
-    five_years = date - datetime.timedelta(weeks=(52*5))
+    five_years = date - datetime.timedelta(weeks=(52 * 5))
     for issue in issues:
         if issue.state == query:
             created_at = issue.created_at
