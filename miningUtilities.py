052ecd1f99ca239e4c785d64106c6cdff2bb2019
Stephanie <steph99rodriguez@gmail.com>
2021-04-05 20:35:48 -0500

    more comments explaining how code works

--- miningUtilities.py
+++ miningUtilities.py
@@ -9,11 +9,16 @@ import pandas as pd # pip install pandas
 GETTING STARTED WITH PYGITHUB 
 
 from github import Github
-# First create a Github instance:
-# using an access token
+# First create a Github instance using an access token: 
 g = Github("access_token")
 # Then play with your Github objects:
 repo = g.get_repo("matplotlib/matplotlib"):
+
+WHEN CREATING PERSONAL ACCESS TOKEN
+Go to github.com/settings/tokens
+Click on Generate new token
+Select public_repo 
+Copy token into code below 
 '''
 
 def main():
@@ -49,31 +54,29 @@ def get_all_issues(repo):
     issues = repo.get_issues(state="all", sort="created")
     return issues
 
-
 def get_issues_over_time(issues):
     """
-    Return a dictionary in which each key represents a timeframe (day, week, month, year, 5 years)
-    and holds the value of a list of issues that have been created within that timeframe
     Sends information obtained from API to a CSV file 
 
         Parameters:
-                issues (PaginatedList of Issues): the repositories' issues
-                query (str): The state to be queries e.g "open"/"closed"
+            issues (PaginatedList of Issues): the repositories' issues
+        Returns:
+            None, instead, prints out csv 
 
     """
-    time = {
-        "day": list(),
-        "week": list(),
-        "month": list(),
-        "year": list(),
-        "five_years": list()
-    }
-    date = datetime.datetime.today()
-    day = date - datetime.timedelta(days=1)
-    week = date - datetime.timedelta(weeks=1)
-    month = date - datetime.timedelta(weeks=4)
-    year = date - datetime.timedelta(weeks=52)
-    five_years = date - datetime.timedelta(weeks=(52 * 5))
+    # time = {
+    #     "day": list(),
+    #     "week": list(),
+    #     "month": list(),
+    #     "year": list(),
+    #     "five_years": list()
+    # }
+    # date = datetime.datetime.today()
+    # day = date - datetime.timedelta(days=1)
+    # week = date - datetime.timedelta(weeks=1)
+    # month = date - datetime.timedelta(weeks=4)
+    # year = date - datetime.timedelta(weeks=52)
+    # five_years = date - datetime.timedelta(weeks=(52 * 5))
     issue_columns = ["Issue ID", "Title", "Body", "User", "State","Created At", "Assignees", "Closed At", "Closed By", "Updated At", "Number of Comments", "Labels", "Milestone Title", "Milestone Number"]
     all_issues = pd.DataFrame()
 
