56f1a60602440ffd24a8e785dd1d9e1182a8b2d5
Dylan Klintworth <dylan.klintworth@gmail.com>
2021-04-11 23:23:51 -0500

    added commit data spreadsheet

--- Github3Utilities.py
+++ Github3Utilities.py
@@ -26,6 +26,14 @@ def commits_to_csv(commits_list, path, file_exists):
         time = str(commit.commit.committer.get('date'))
         date = datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
         date = date.strftime("%m/%d/%Y %I:%M:%S %p")
+        tree = commit.commit.tree.to_tree().recurse().as_dict().get('tree')
+        code_size = 0
+        for t in tree:
+            size = str(t.get('size'))
+            if not size == "None":
+                code_size += int(size)
+            else:
+                continue
         if commit.author:
             author = str(commit.author.login)
         else:
@@ -35,19 +43,14 @@ def commits_to_csv(commits_list, path, file_exists):
             "message": message,
             "author": author,
             "date": date,
-            "url": url
+            "url": url,
+            "code_size": code_size
         }
         dict_list.append(temp)
     with codecs.open(path, mode='a+', encoding='utf-8') as csv_file:
-        fieldnames = ["sha", "message", "author", "date", "url"]
+        fieldnames = ["sha", "message", "author", "date", "url", "code_size"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         if not file_exists:
             writer.writeheader()
         for index in dict_list:
             writer.writerow(index)
-
-
-if __name__ == "__main__":
-    gh = github3.login()  # enter uname/passwd as arguments
-    repo = get_repo(gh)
-    commits = get_all_commits(repo)
